96f4843b1a3594d1680e17db62a5ab896eaddc1d
davidinmichael <davidinmichael@gmail.com>
2024-03-07 13:04:28 +0100

    Cors handled

--- utility_bill/settings.py
+++ utility_bill/settings.py
@@ -17,7 +17,9 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
+CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
 
 
 # Application definition
