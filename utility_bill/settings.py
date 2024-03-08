117538dd01e81a386b27864713dece2424706319
davidinmichael <davidinmichael@gmail.com>
2024-03-08 12:57:12 +0100

    I created the accounts app

--- utility_bill/settings.py
+++ utility_bill/settings.py
@@ -32,7 +32,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django.contrib.sessions',
 
     "rest_framework",
     'rest_framework_simplejwt',
