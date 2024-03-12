8ccbc2bed22cad740609827a93d9183263980189
davidinmichael <davidinmichael@gmail.com>
2024-03-12 13:39:30 +0100

    Set up and tested templates on the web.

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.views import APIView
 
-# Create your views here.
+
+class AccountView(APIView):
+
+    def get(self, request):
+        context = {
+            "name": "David",
+            "stack": "Backend",
+        }
+        return render(request, "accounts/welcome.html", context)
