626241d06623a0d1f4b63a11f8ecf5b57522425d
davidinmichael <davidinmichael@gmail.com>
2024-03-13 16:15:57 +0100

    Django reload set up

--- utility_bill/settings.py
+++ utility_bill/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     "rest_framework",
     'rest_framework_simplejwt',
     "corsheaders",
+    "django_browser_reload",
 
     "accounts",
 ]
@@ -49,6 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django_browser_reload.middleware.BrowserReloadMiddleware",
 ]
 
 ROOT_URLCONF = 'utility_bill.urls'
--- utility_bill/urls.py
+++ utility_bill/urls.py
@@ -21,7 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("account", include("accounts.urls")),
+    path("__reload__/", include("django_browser_reload.urls")),
+    path("account/", include("accounts.urls")),
 ]
 
 if settings.DEBUG:
